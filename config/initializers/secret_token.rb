8d2e23f7641e563880e5f8499de7f789aec7a7c1
hamitturkukaya <htkaya90@gmail.com>
2013-06-21 12:06:47 +0300

    upload app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tolksample::Application.config.secret_token = '6be64aff95b41612a2c622670a112329e3ffc0366ce23c23c5eee4b4d8caa1a6c45fd14ae980148e160527a01d1b1defd7b5572d8b364a7981cf6e1a245cdcb1'
